25a34ea8eb8ac2a7d3fcb808f4bb521c4a07c206
Arman Ommid <armanommid@gmail.com>
2023-09-27 23:08:23 -0700

    trying cicd

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -2,6 +2,10 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${DIR}/../source/variables.sh
 
+if [[ ! -z "$1" ]]; then
+    BASE_REPO_TAG=$1
+fi
+
 set -x # Echo command lines before executing; set +x = off
 docker build \
     --pull \
--- docker/dev/build.sh
+++ docker/dev/build.sh
@@ -2,6 +2,18 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${DIR}/../source/variables.sh
 
+if [[ ! -z "$1" ]]; then
+    BASE_REPO_TAG=$1
+fi
+
+if [[ ! -z "$2" ]]; then
+    IMAGE_REPO_TAG=$2
+fi
+
+if [[ ! -z "$3" ]]; then
+    PROJECT_NAME=$3
+fi
+
 set -x # Echo command lines before executing; set +x = off
 docker build \
     --build-arg BASE_REPO_TAG=${BASE_REPO_TAG} \
